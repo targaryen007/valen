fb4fc6b4e7176658e92ccd701d864f1b37e22b34
targaryen007 <39377020+targaryen007@users.noreply.github.com>
2026-02-03 20:18:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const noBtn = document.getElementById("noBtn");
+const area = document.querySelector(".button-area");
+
+noBtn.addEventListener("mouseenter", () => {
+  const maxX = area.clientWidth - noBtn.offsetWidth;
+  const maxY = area.clientHeight - noBtn.offsetHeight;
+
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+function yesClick() {
+  document.body.innerHTML = `
+    <div style="
+      height:100vh;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      flex-direction:column;
+      background:#ffe6eb;
+      font-family:Arial;
+      text-align:center;
+    ">
+      <h1 style="color:#ff4d6d;">💘 I knew it! 💘</h1>
+      <p style="font-size:22px;">I knew you would say YES 😌❤️</p>
+    </div>
+  `;
+}
